6cf58933c7a0b6677c907d1ed73cb5c32a349f04
Born2Ride <30387055+ccirelli2@users.noreply.github.com>
2020-04-14 20:17:38 -0400

    Delete prophet_tutorial.py

--- prophet_tutorial.py
+++
@@ -1,8 +0,0 @@
-## DOCUMENTATION ______________________________________________
-''' Desc:
-    Ref: https://www.analyticsvidhya.com/blog/2018/05/generate-accurate-forecasts-facebook-prophet-python-r/
-
-'''
-
-
-
