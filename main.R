1e020f2aa6190653bca03124e5d2b0d12e4a777f
cc2 <ccirelli2@gmail.com>
2020-04-16 11:27:38 -0400

    -q

--- main.R
+++ main.R
@@ -86,6 +86,41 @@ plt.hour  <- ggplot(hour.cnt, aes(x=Hour, y=n)) + geom_bar(stat='identity') + gg
 plt.month
 plot(x=seq(1,length(month.cnt$n)), y=month.cnt$n, 'l')
 
+# Stationarity --------------------------------------------------------------------------------------------
+ts.12 <- ts(date.cnt$n, start=c(2009, 01 ,01), end=c(2019, 12,31), frequency=12)
+monthplot(ts.12, main='Stationarity Test - Monthly Plot of Mean Count - Frequency =12')
+ts.6 <- ts(date.cnt$n, start=c(2009, 01 ,01), end=c(2019, 12,31), frequency=6)
+monthplot(ts.6, main='Stationarity Test - Monthly Plot of Mean Count - Frequency =6')
+
+#****** Take Log of data to see difference in mean. 
+x         <- seq(1, length(ts.12))
+ts.12.log <- log(ts.12)
+ts.6.log  <- log(ts.6)
+
+par(mfrow=c(2,1))
+plot(x=x, y=ts.12, col='green', type='l')
+plot(x=x, y=ts.12.log, col='red', type='l')
+
+x         <- seq(1, length(ts.6))
+plot(x=x, y=ts.6, col='green', type='l')
+plot(x=x, y=ts.6.log, col='red', type='l')
+
+# Check if variance is constant
+par(mfrow=c(1,1))
+h1 = ts.6[1: round(length(ts.6 *.5))]
+h2 = ts.6[36: length(ts.6)]
+v1 = var(h1)
+v2 = var(h2)
+?barplot
+barplot(height=c(v1, v2), names.arg=c('v1', 'v2'), main='Compare Varianc e- 1st Half vs 2nd Half')
+
+
+# Take Log and Check Variance Again
+h1.log <- log(h1)
+h2.log <- log(h2)
+v1.log <- var(h1.log)
+v2.log <- var(h2.log)
+barplot(height=c(v1.log, v2.log), names.arg=c('v1', 'v2'), main='Compare Varianc - Log 1st Half vs 2nd Half')
 
 
 # Seasonality ---------------------------------------------------------------------------------------------
@@ -99,7 +134,7 @@ viz_season_v1(date.cnt, 6)
 # Take Difference to remove trend -------------------------------------------------------------------------
 ' Observations:  No material differences in plot.  Still difficult to vizualize. 
 '
-viz_season_v2(date.cnt.diff, 31)
+viz_season_v2(date.cnt, 31)
 
 
 # Apply Smoothing to Differenced Time Series --------------------------------------------------------------
@@ -129,31 +164,82 @@ gen_seasonal_lots(decomp, date.cnt, 6, 9)
 
 
 
-
 # Stationarity - ACF & PACF Plots -------------------------------------------------------------------------
 
-# Dickey Fuller Test
-'	Augmented Dickey-Fuller Test
-  data:  date.cnt$n
-  Dickey-Fuller = -48.161, Lag order = 0, p-value = 0.01
-  alternative hypothesis: stationary
-'
-adf.test(date.cnt$n, k=0)
+# Create Time Series 
+ts.6 <- ts(date.cnt$n, start=c(2009, 01 ,01), end=c(2019, 12,31), frequency=6)
+ts.12  <- ts(date.cnt$n, start=c(2009, 01 ,01), end=c(2019, 12,31), frequency=12)
 
+
+
+# Dickey Fuller Test
+' Observations:
+    Unclearn why the DF test says that the data is stationary when we know
+    that it is not. 
+    
+  Output:
+    Augmented Dickey-Fuller Test
+    data:  date.cnt$n
+    Dickey-Fuller = -48.161, Lag order = 0, p-value = 0.01
+    alternative hypothesis: stationary
+'
+adf.test(ts.6, k=0)
 
 # ACF & PACF Plot 
-' ACF:  
+' Observations: The plots indicate that this is possibly a 032-130-6 process. 
 '
-ts <- ts(date.cnt$n, start=c(2009, 01 ,01), end=c(2019, 12,31), frequency=12)
-ts.diff <- diff(ts, 1)
-
-acf(ts.diff)
-pacf(ts.diff)
-
-plot(decompose(ts))
+par(mfrow=c(1,1))
+ts.6.diff <- diff(ts.6, 3)
+acf(ts.6.diff)
+pacf(ts.6.diff)
 
 
+# Utilize Auto Arima to verify best arima order -----------------------------------------------------------------
+
+# Fit Auto Arima Model
+' auto.arima.6  = Best model: ARIMA(0,0,0) with non-zero mean 
+  auto.arima.12 = Best model: ARIMA(0,0,1) with non-zero mean' 
+auto.arima.6  <- auto.arima(ts.6, trace=TRUE, ic='bic')
+auto.arima.12 <- auto.arima(ts.12, trace=TRUE, ic='bic')
 
 
+# Fit Auto Arima - Use Log of 
+' auto.arima.6  = Best model: ARIMA(0,1,1)   
+  auto.arima.12 = Best model: ARIMA(0,0,0)  
+'
+ts.6.log     <- log(ts.6)
+ts.12.log    <- log(ts.12)
+auto.arima.6 <- auto.arima(ts.6.log, trace=TRUE, ic='bic')
+auto.arima.12 <- auto.arima(ts.12.log, trace=TRUE, ic='bic')
 
 
+# Manually Fit SARIMA Model ---------------------------------------------------------------------------------------
+' Approach:      Try multiple fits
+  Observation:   sarima (0,1,2)(2,2,1)x6 provided the lowest aic score. 
+                 the prediction verus the plot results are pretty good. 
+'
+
+# Train Test Split  
+train.sample <- round(0.7 * length(ts.6.log))
+test.sample  <- length(ts.6.log) - train.sample
+
+ts.train <-  ts.6[1: train.sample]
+ts.test  <-  ts.6[51: length(ts.6)]
+
+# Train Sarima Model (Order = 032 130 6)
+sarima.6.fit  <- arima(ts.train, order=c(0,1,2), seasonal = list(order=c(2,2,1), period=6))
+summary(sarima.6.fit)
+sarima.6.pred <- predict(sarima.6.fit, length(ts.test))
+
+# Plot Prediction Vs Actual
+x = seq(1, test.sample)
+plot(x=x, y=sarima.6.pred$pred, col='blue', type='l', xlab='Time', ylab='Count Auto Theft', main='SARIMA (0,1,2)(2,2,1)x6 - Prediction vs Actual')
+lines(x=x, y=ts.test, col='red')
+legend(1, 5, legend=c('Actual', 'Predicted'), col=c('Blue', 'Red'), lty=1:2, cex=0.8)
+
+# Calculate RMSE
+sarima.6.mse <- sum((sarima.6.pred$pred - ts.test)**2) / length(sarima.6.pred$pred)
+sarima.6.mse
+summary(sarima.6.fit)
+
+sum((sarima.6.pred$pred - ts.test)**2) / length(ts.test)
